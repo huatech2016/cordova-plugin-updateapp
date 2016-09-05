893ebcc906dd8ee7e59bd0c1432324922550570c
huatech2016 <libo@huatech.net>
2016-09-05 16:57:59 +0800

    Update updateAppPlugin.js

--- www/updateAppPlugin.js
+++ www/updateAppPlugin.js
@@ -1,9 +1,11 @@
-cordova.define("com.qinsilk.updateapp.updateAppPlugin", function(require, exports, module) {
-/**
- * 检查并更新APP
- * @author robin
- */
-function UpdateApp() {}
+cordova.define("com.qinsilk.updateapp.updateAppPlugin", function (require, exports, module) {
+    /**
+     * 检查并更新APP
+     * @author robin
+     */
+    function UpdateApp() {
+    }
+
 // UpdateApp.prototype.checkAndUpdate = function(checkPath) {
 // 	cordova.exec(null, null, "UpdateApp", "checkAndUpdate", [checkPath]);
 // }
@@ -13,20 +15,20 @@ function UpdateApp() {}
 // UpdateApp.prototype.getServerVerInfo = function(successCallback, failureCallback, checkPath) {
 // 	cordova.exec(successCallback, failureCallback, "UpdateApp", "getServerVersion", [checkPath]);
 // }
+    
 // UpdateApp.prototype.getAppVersion = function(successCallback, failureCallback) {
 // 	cordova.exec(successCallback, failureCallback, "UpdateApp", "getVersionName", []);
 // }
-UpdateApp.prototype.hasNewVersion = function(successCallback, failureCallback,args) {
- 	cordova.exec(successCallback, failureCallback, "UpdateApp", "hasNewVersion", args);
-}
-UpdateApp.prototype.downloadApk = function(successCallback, failureCallback,args) {
- 	cordova.exec(successCallback, failureCallback, "UpdateApp", "downloadApk", args);
-}
-cordova.addConstructor(function() {
-	if (!window.plugins) {
-		window.plugins = {};
-	}
-	window.plugins.updateApp = new UpdateApp();
-});
-
+    UpdateApp.prototype.hasNewVersion = function (successCallback, failureCallback, checkPath) {
+        cordova.exec(successCallback, failureCallback, "UpdateApp", "hasNewVersion", [checkPath]);
+    }
+    UpdateApp.prototype.downloadApk = function (successCallback, failureCallback, checkPath) {
+        cordova.exec(successCallback, failureCallback, "UpdateApp", "downloadApk", [checkPath]);
+    }
+    cordova.addConstructor(function () {
+        if (!window.plugins) {
+            window.plugins = {};
+        }
+        window.plugins.updateApp = new UpdateApp();
+    });
 });
