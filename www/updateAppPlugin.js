568ff581839c137a963dfc005609c78f985e62fe
huatech2016 <libo@huatech.net>
2016-09-05 10:57:05 +0800

    Update updateAppPlugin.js

--- www/updateAppPlugin.js
+++ www/updateAppPlugin.js
@@ -3,17 +3,23 @@
  * @author robin
  */
 function UpdateApp() {}
-UpdateApp.prototype.checkAndUpdate = function(checkPath) {
-	cordova.exec(null, null, "UpdateApp", "checkAndUpdate", [checkPath]);
+// UpdateApp.prototype.checkAndUpdate = function(checkPath) {
+// 	cordova.exec(null, null, "UpdateApp", "checkAndUpdate", [checkPath]);
+// }
+// UpdateApp.prototype.getCurrentVerInfo = function(successCallback) {
+// 	cordova.exec(successCallback, null, "UpdateApp", "getCurrentVersion", []);
+// }
+// UpdateApp.prototype.getServerVerInfo = function(successCallback, failureCallback, checkPath) {
+// 	cordova.exec(successCallback, failureCallback, "UpdateApp", "getServerVersion", [checkPath]);
+// }
+// UpdateApp.prototype.getAppVersion = function(successCallback, failureCallback) {
+// 	cordova.exec(successCallback, failureCallback, "UpdateApp", "getVersionName", []);
+// }
+UpdateApp.prototype.getAppVersion = function(successCallback, failureCallback,checkPath) {
+ 	cordova.exec(successCallback, failureCallback, "UpdateApp", "hasNewVersion", checkPath);
 }
-UpdateApp.prototype.getCurrentVerInfo = function(successCallback) {
-	cordova.exec(successCallback, null, "UpdateApp", "getCurrentVersion", []);
-}
-UpdateApp.prototype.getServerVerInfo = function(successCallback, failureCallback, checkPath) {
-	cordova.exec(successCallback, failureCallback, "UpdateApp", "getServerVersion", [checkPath]);
-}
-UpdateApp.prototype.getAppVersion = function(successCallback, failureCallback) {
-	cordova.exec(successCallback, failureCallback, "UpdateApp", "getVersionName", []);
+UpdateApp.prototype.getAppVersion = function(successCallback, failureCallback,checkPath) {
+ 	cordova.exec(successCallback, failureCallback, "UpdateApp", "downloadApk", checkPath);
 }
 cordova.addConstructor(function() {
 	if (!window.plugins) {
